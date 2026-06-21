2db825c0287e3468a2a0abebbf0feb65cd5ae344
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-06-21 21:52:11 +0600

    feat: parse request and handle GET

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ HOST = ""
 PORT = 8080
 
 
+def prepare_get_response() -> str:
+    body = "nothing to get from this server."
+    content_length = len(body.encode())
+    response = (
+        "HTTP/1.1 200 OK\r\n"
+        + "Content-Type:text/plain\r\n"
+        + f"Content-Length:{content_length}\r\n\r\n{body}"
+    )
+    return response
+
+
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind((HOST, PORT))
@@ -13,7 +24,13 @@ def main():
             print("connected by ", addr)
             data = conn.recv(512)
             if data:
-                conn.sendall(data)
+                list = data.split(sep=b"\r\n")
+                status = list[0].split(b" ")
+
+                if status[0] == b"GET":
+                    print("start processing GET request")
+                    response = prepare_get_response()
+                    conn.sendall(response.encode())
 
 
 if __name__ == "__main__":
